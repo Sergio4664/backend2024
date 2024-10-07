05e8084d969ebbd9f93680582ded0b235de79f16
Sergio Cervantes Guatemala <sergiocervantes742@gmail.com>
2024-10-07 12:01:25 -0600

    control_usuarios

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -2,22 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.get("/usuarios",(req,res)=>{
-    const usuarios = [
-      { id: 1,
-        nombre: "Irving",
-        apellidos: "Vital",
-        email: "irvingenrique164@gmail.com",
-      },
+const usuarios = [
+  { id: 1,
+    nombre: "Sergio",
+    apellidos: "Cervantes",
+    email: "sergiocervantes742@gmail.com",
+  },
 
-      { id: 2,
-        nombre: "Sergio",
-        apellidos: "Cervantes",
-        email: "irvingenrique164@gmail.com",
-      },
-    ];
-    res.status(200).send(usuarios);
+  { id: 2,
+    nombre: "Sergio",
+    apellidos: "Cervantes",
+    email: "vitalxd@gmail.com",
+  },
+];
+
+app.get("/usuarios",(req,res)=>{
+    
+  res.status(200).send(usuarios);
 });
+
+app.get("/usuarios/:id", (req, res) => {
+const {id} = req.params;
+  const usuario = usuarios.find((usuario)=> usuario.id === +id);
+  res.status(200).send(usuario);
+});//end point
 
 app.listen(3000, () => {
     console.log("Servidor cooriendo en http://localhost:3000")
