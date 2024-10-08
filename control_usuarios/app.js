f4201cffdb30fa280ab6bbe94dcb00b617f7d672
Sergio Cervantes Guatemala <sergiocervantes742@gmail.com>
2024-10-08 11:54:24 -0600

    control_usuarios

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -23,7 +23,19 @@ app.get("/usuarios",(req,res)=>{
 
 app.get("/usuarios/:id", (req, res) => {
 const {id} = req.params;
+
+if(isNaN(id)){
+  res.status(400).send({error: "El id debe ser un número"});
+  return;
+}
+
   const usuario = usuarios.find((usuario)=> usuario.id === +id);
+
+  if(usuario === undefined){
+    res.status(404).send({error: `El usuario con id ${id} no existe`});
+  return;
+  };
+
   res.status(200).send(usuario);
 });//end point
 
