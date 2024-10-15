3547dd2bf9b3ec8f2c81cd1238e5375107c09ad0
Sergio Cervantes Guatemala <sergiocervantes742@gmail.com>
2024-10-15 11:17:22 -0600

    control_usuarios

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -2,17 +2,19 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const usuarios = [
   { id: 1,
-    nombre: "Sergio",
-    apellidos: "Cervantes",
-    email: "sergiocervantes742@gmail.com",
+    nombre: "Irving",
+    apellidos: "Vital",
+    email: "irvingenrique164@gmail.com",
   },
 
   { id: 2,
     nombre: "Sergio",
     apellidos: "Cervantes",
-    email: "vitalxd@gmail.com",
+    email: "cervantes2000@gmail.com",
   },
 ];
 
@@ -22,22 +24,48 @@ app.get("/usuarios",(req,res)=>{
 });
 
 app.get("/usuarios/:id", (req, res) => {
-const {id} = req.params;
-
-if(isNaN(id)){
-  res.status(400).send({error: "El id debe ser un número"});
-  return;
-}
-
-  const usuario = usuarios.find((usuario)=> usuario.id === +id);
-
-  if(usuario === undefined){
-    res.status(404).send({error: `El usuario con id ${id} no existe`});
-  return;
+  const {id} = req.params;
+  
+  if(isNaN(+id)) {
+  res.status(400).send({error: "El id debe ser número"});
+  return
   };
 
+  //console.log(typeof +id);
+  //console.log(params);
+
+  const usuario = usuarios.find((usuario) => usuario.id === +id);
+
+  if(usuario === undefined){
+    res.status(400).send({error: `El usuario con id ${id} no existe`});
+    return;
+  };
+  
   res.status(200).send(usuario);
-});//end point
+
+});
+
+app.post("/usuarios",(req, res) => {
+  const {nombre, apellidos, email} = req.body;
+
+    // Validar que todos los campos estén presentes
+    if (!nombre || !apellidos || !email) {
+      res.status(404).send({ error: "Todos los campos (nombre, apellidos, email) son obligatorios" });
+      return;
+    }
+  // Validar que el email no se repita
+  const emailExistente = usuarios.find((usuario) => usuario.email === email);
+  if (emailExistente) {
+    res.status(400).send({ error: "El correo electrónico ya está registrado" });
+    return;
+  }
+  
+  usuarios.push({id: usuarios.length +1, nombre, apellidos, email});
+  
+  //console.log(req.body);
+  res.status(201).send("El usuario se agregó correctamente");
+
+});
 
 app.listen(3000, () => {
     console.log("Servidor cooriendo en http://localhost:3000")
